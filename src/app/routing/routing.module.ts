22a99df028e351f6725735b5bb101f0798ab12ed
Tarik Guney <tarik.guney@motorolasolutions.com>
2019-01-07 13:18:52 -0700

    Added routing module

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router'
+
+import { MainComponent } from "../main/main.component"
+
+// Add more routes here...
+const routes: Routes = [
+  { path: "home", component: MainComponent },
+  { path: "", redirectTo: '/home', pathMatch: 'full' }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class RoutingModule { }
