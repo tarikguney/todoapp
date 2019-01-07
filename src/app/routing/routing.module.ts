237643edc6e2618ea0c0c6a35b786b88337cc91c
Tarik Guney <tarik.guney@motorolasolutions.com>
2019-01-07 13:21:36 -0700

    Fixed the routing module after changing the directory of the main component

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'
 
-import { MainComponent } from "../main/main.component"
+import { MainComponent } from "../todo/main/main.component"
 
 // Add more routes here...
 const routes: Routes = [
